061b2898b77a1855a5eb295d408ebd3ff0f2c563
Nick Farina <nfarina@gmail.com>
2011-10-13 11:00:44 -0700

    AccountCell stub.

---
+++ Feeds/AccountCell.h
@@ -0,0 +1,13 @@
+//
+//  AccountCell.h
+//  Feeds
+//
+//  Created by Nick Farina on 10/13/11.
+//  Copyright (c) 2011 Spotlight Mobile. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface AccountCell : NSTextFieldCell
+
+@end
---
+++ Feeds/AccountCell.m
@@ -0,0 +1,13 @@
+//
+//  AccountCell.m
+//  Feeds
+//
+//  Created by Nick Farina on 10/13/11.
+//  Copyright (c) 2011 Spotlight Mobile. All rights reserved.
+//
+
+#import "AccountCell.h"
+
+@implementation AccountCell
+
+@end
