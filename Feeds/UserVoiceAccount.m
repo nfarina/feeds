d68b090bcac6b13e0be3672aa20c1052a8d43837
Nick Farina <nfarina@gmail.com>
2012-03-28 09:26:29 -0700

    Memory leak fix.

--- Feeds/UserVoiceAccount.m
+++ Feeds/UserVoiceAccount.m
@@ -18,7 +18,7 @@
 
 - (void)forumRequestComplete:(NSData *)data {
     
-    TFHpple *html = [[TFHpple alloc] initWithHTMLData:data];
+    TFHpple *html = [[[TFHpple alloc] initWithHTMLData:data] autorelease];
     NSArray *links = [html searchWithXPathQuery:@"//link[@type='application/atom+xml']"];
     
     NSMutableArray *foundFeeds = [NSMutableArray array];
