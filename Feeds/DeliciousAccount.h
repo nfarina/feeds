eddf9c1035499c6ad15fbc528610ad2975a29267
Tobias Tom <t.tom@succont.de>
2013-05-09 15:57:51 +0200

    Added initial delicious implementation.

---
+++ Feeds/DeliciousAccount.h
@@ -0,0 +1,13 @@
+//
+//  DeliciousAccount.h
+//  Feeds
+//
+//  Created by Tobias Tom on 09.05.13.
+//  Copyright (c) 2013 Tobias Tom. All rights reserved.
+//
+
+#import "Account.h"
+
+@interface DeliciousAccount : Account
+
+@end
